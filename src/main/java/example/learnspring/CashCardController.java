8f6b6e7002e4123e439b4884a7608d8fdb1da483
fieTarigan <ofieisranta@gmail.com>
2024-08-19 17:01:01 +0700

    Feat: finisih spring security before csrf

--- src/main/java/example/learnspring/CashCardController.java
+++ src/main/java/example/learnspring/CashCardController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,23 +25,33 @@ public class CashCardController {
     }
 
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId, Principal principal) {
+        System.out.println("requestedId : " + requestedId + "principal.getName() : " + principal.getName());
+        Optional<CashCard> cashCardOptional = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
 
-        return cashCardOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        System.out.println("cashCardOptional.isPresent() : " + cashCardOptional.isPresent());
+
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
-    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
-        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb, Principal principal) {
+        CashCard cashCardWithOwner = new CashCard(null, newCashCardRequest.amount(), principal.getName());
+
+        CashCard savedCashCard = cashCardRepository.save(cashCardWithOwner);
         URI locationOfNewCashCard = ucb.path("/cashcards/{requestedId}").buildAndExpand(savedCashCard.id()).toUri();
 
         return ResponseEntity.status(HttpStatus.CREATED).header(HttpHeaders.LOCATION, locationOfNewCashCard.toASCIIString()).build();
     }
 
     @GetMapping
-    private ResponseEntity<Iterable<CashCard>> findAll(Pageable pageable) {
-        Page<CashCard> page = cashCardRepository.findAll(
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable, Principal principal) {
+        Page<CashCard> page = cashCardRepository.findByOwner(
+                principal.getName(),
                 PageRequest.of(
                         pageable.getPageNumber(),
                         pageable.getPageSize(),
--- src/test/java/example/learnspring/CashCardJsonTest.java
+++ src/test/java/example/learnspring/CashCardJsonTest.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
-public class CashCardJsonTest {
+class CashCardJsonTest {
 
     @Autowired
     private JacksonTester<CashCard> json;
@@ -25,9 +25,9 @@ public class CashCardJsonTest {
     @BeforeEach
     public void setUp() {
         cashCards = Arrays.array(
-                new CashCard(99L, 123.45),
-                new CashCard(100L, 1.00),
-                new CashCard(101L, 150.00)
+                new CashCard(99L, 123.45, "sarah1"),
+                new CashCard(100L, 1.00, "sarah1"),
+                new CashCard(101L, 150.00, "sarah1")
         );
     }
 
@@ -48,11 +48,12 @@ public class CashCardJsonTest {
         String content = """
                 {
                     "id": 99,
-                    "amount": 123.45
+                    "amount": 123.45, 
+                    "owner": "sarah1"
                 }
                 """;
         CashCard cashCard = json.parse(content).getObject();
-        assertThat(cashCard).isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parse(content)).isEqualTo(new CashCard(99L, 123.45, "sarah1"));
         assertThat(cashCard.id()).isEqualTo(99);
         assertThat(cashCard.amount()).isEqualTo(123.45);
     }
@@ -66,9 +67,10 @@ public class CashCardJsonTest {
     void cashCardListDesererializationTest() throws IOException {
         String content = """
                 [
-                    { "id": 99, "amount": 123.45 },
-                    { "id": 100, "amount": 1.00 },
-                    { "id": 101, "amount": 150.00 }
+                     {"id": 99, "amount": 123.45 , "owner": "sarah1"},
+                     {"id": 100, "amount": 1.00 , "owner": "sarah1"},
+                     {"id": 101, "amount": 150.00, "owner": "sarah1"}
+                                                  
                 ]
                 """;
         CashCard[] cashCards = jsonList.parse(content).getObject();
